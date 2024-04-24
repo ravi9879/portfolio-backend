facc9e3126383fb838c11c32ae74cb8eace73300
ravi9879 <144209099+ravi9879@users.noreply.github.com>
2024-04-24 17:17:23 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 // const mURL = "mongodb://localhost:27017/";
-const mURL = "mongodb+srv://ravigupta77389:Ravi$500@cluster0.3nuxwpq.mongodb.net/" ;
+// const mURL = "mongodb+srv://ravigupta77389:Ravi$500@cluster0.3nuxwpq.mongodb.net/" ;
+const mURL = "mongodb+srv://ravigupta77389:Ravi500@ravip.d9vmqop.mongodb.net/?retryWrites=true&w=majority&appName=ravip"  ;
 const mconnect = () => {
     if (mongoose.connect(mURL)) {
         console.log("connected");
